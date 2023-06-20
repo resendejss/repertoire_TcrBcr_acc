52e02cd6250e9f1d121c7440a50bbbbc7ece4d40
resendejss <jean.souza@edu.unipar.br>
2023-06-20 10:26:52 -0300

    atualizacao

--- 4_analyzes_after_extraction_of_TCR_and_BCR/2_expression_IG_TCR/expression_TCR_BCR_boxplot.R
+++ 4_analyzes_after_extraction_of_TCR_and_BCR/2_expression_IG_TCR/expression_TCR_BCR_boxplot.R
@@ -3,7 +3,7 @@
 #                                                                              #
 # Author: Jean Resende (jean.s.s.resende@gmail.com)                            #
 # Creation date: 2023/06/19                                                    #
-# Last update date: 2023/06/19                                                 #
+# Last update date: 2023/06/20                                                 #
 ################################################################################
 library(ggplot2)
 library(ggpubr)
@@ -23,20 +23,30 @@ identical(rownames(v.counts), rownames(j.counts))
 
 data_vdj <- v.counts + d.counts + j.counts
 
-
 identical(rownames(data_vdj), metadata$sample_id)
 idx <- match(rownames(data_vdj), metadata$sample_id)
 identical(rownames(data_vdj), metadata$sample_id[idx])
 
 data_vdj$steroid <- metadata$steroid[idx]
-data_vdj_log2 <- data_vdj
-data_vdj_log2[,1:7] <- log2(data_vdj_log2[,1:7]+ 1)
+data_vdj_BT <- data_vdj
+data_vdj_BT$b <- data_vdj_BT$IGK + data_vdj_BT$IGL + data_vdj_BT$IGH
+data_vdj_BT$t <- data_vdj_BT$TRB + data_vdj_BT$TRA + data_vdj_BT$TRG + data_vdj_BT$TRD
+
+data_vdj_BT$IGK[1] + data_vdj_BT$IGL[1] + data_vdj_BT$IGH[1]
+data_vdj_BT$b[1]
+data_vdj_BT$TRB[1] + data_vdj_BT$TRA[1] + data_vdj_BT$TRG[1] + data_vdj_BT$TRD[1]
+data_vdj_BT$t[1]
+
+data_vdj_BT$bnorm <- data_vdj_BT$b/metadata$reads[idx]
+data_vdj_BT$tnorm <- data_vdj_BT$t/metadata$reads[idx]
+
+data_vdj_log2 <- data_vdj_BT
+data_vdj_log2[,9:12] <- log2(data_vdj_log2[,9:12]+ 1)
 
 data_vdj_log2 <- na.omit(data_vdj_log2)
 
-
-g1 <- ggplot(data_vdj_log2, aes(x=steroid, y=IGK, fill=steroid))+
-  ggtitle("IGK")+
+g1 <- ggplot(data_vdj_log2, aes(x=steroid, y=b, fill=steroid))+
+  ggtitle("Non-normalized BCR")+
   geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
   scale_y_continuous("IGL counts")+
   scale_x_discrete("")+
@@ -59,12 +69,12 @@ g1 <- ggplot(data_vdj_log2, aes(x=steroid, y=IGK, fill=steroid))+
         legend.box = "horizontal" )+
   stat_compare_means(method="wilcox.test", 
                      label.x = c(1.3), 
-                     label.y = 16.1,
+                     label.y = 17,
                      label="p.format",
                      size=3)
 
-g2 <- ggplot(data_vdj_log2, aes(x=steroid, y=IGL, fill=steroid))+
-  ggtitle("IGL")+
+g2 <- ggplot(data_vdj_log2, aes(x=steroid, y=t, fill=steroid))+
+  ggtitle("Non-normalized TCR")+
   geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
   scale_y_continuous("IGL counts")+
   scale_x_discrete("")+
@@ -87,14 +97,14 @@ g2 <- ggplot(data_vdj_log2, aes(x=steroid, y=IGL, fill=steroid))+
         legend.box = "horizontal" )+
   stat_compare_means(method="wilcox.test", 
                      label.x = c(1.3), 
-                     label.y = 16.1,
+                     label.y = 17,
                      label="p.format",
                      size=3)
 
-g3 <- ggplot(data_vdj_log2, aes(x=steroid, y=IGH, fill=steroid))+
-  ggtitle("IGH")+
+g3 <- ggplot(data_vdj_log2, aes(x=steroid, y=bnorm, fill=steroid))+
+  ggtitle("Normalized BCR")+
   geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
-  scale_y_continuous("IGH counts")+
+  scale_y_continuous("expression BCR")+
   scale_x_discrete("")+
   scale_fill_manual(values=c("#F8766D","#00BFC4"), 
                     labels=c("HSP (n=45)","LSP (n=30)"))+
@@ -115,14 +125,14 @@ g3 <- ggplot(data_vdj_log2, aes(x=steroid, y=IGH, fill=steroid))+
         legend.box = "horizontal" )+
   stat_compare_means(method="wilcox.test", 
                      label.x = c(1.3), 
-                     label.y = 16.1,
+#                     label.y = 17,
                      label="p.format",
                      size=3)
 
-g4 <- ggplot(data_vdj_log2, aes(x=steroid, y=TRB, fill=steroid))+
-  ggtitle("TRB")+
+g4 <- ggplot(data_vdj_log2, aes(x=steroid, y=tnorm, fill=steroid))+
+  ggtitle("Normalized TCR")+
   geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
-  scale_y_continuous("TRB counts")+
+  scale_y_continuous("expression TCR")+
   scale_x_discrete("")+
   scale_fill_manual(values=c("#F8766D","#00BFC4"), 
                     labels=c("HSP (n=45)","LSP (n=30)"))+
@@ -143,93 +153,13 @@ g4 <- ggplot(data_vdj_log2, aes(x=steroid, y=TRB, fill=steroid))+
         legend.box = "horizontal" )+
   stat_compare_means(method="wilcox.test", 
                      label.x = c(1.3), 
-                     label.y = 16.1,
+#                    label.y = 17,
                      label="p.format",
                      size=3)
 
-g5 <- ggplot(data_vdj_log2, aes(x=steroid, y=TRA, fill=steroid))+
-  ggtitle("TRA")+
-  geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
-  scale_y_continuous("TRA counts")+
-  scale_x_discrete("")+
-  scale_fill_manual(values=c("#F8766D","#00BFC4"), 
-                    labels=c("HSP (n=45)","LSP (n=30)"))+
-  geom_rug(data = data_vdj_log2[data_vdj_log2$steroid=="LSP",], 
-           aes(x=NULL), 
-           sides=  "r",
-           colour= "#00BFC4")+
-  geom_rug(data = data_vdj_log2[data_vdj_log2$steroid=="HSP",], 
-           aes(x=NULL), 
-           sides=  "l",
-           colour="#F8766D")+
-  theme(legend.position = "bottom", 
-        legend.title = element_text(face="bold", size=10),
-        legend.key=element_rect(size=10, color=NA), 
-        legend.key.size=unit(8,"mm"),
-        legend.text=element_text(size=10), 
-        legend.direction = "horizontal",
-        legend.box = "horizontal" )+
-  stat_compare_means(method="wilcox.test", 
-                     label.x = c(1.3), 
-                     label.y = 16.1,
-                     label="p.format",
-                     size=3)
 
-g6 <- ggplot(data_vdj_log2, aes(x=steroid, y=TRG, fill=steroid))+
-  ggtitle("TRG")+
-  geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
-  scale_y_continuous("TRG counts")+
-  scale_x_discrete("")+
-  scale_fill_manual(values=c("#F8766D","#00BFC4"), 
-                    labels=c("HSP (n=45)","LSP (n=30)"))+
-  geom_rug(data = data_vdj_log2[data_vdj_log2$steroid=="LSP",], 
-           aes(x=NULL), 
-           sides=  "r",
-           colour= "#00BFC4")+
-  geom_rug(data = data_vdj_log2[data_vdj_log2$steroid=="HSP",], 
-           aes(x=NULL), 
-           sides=  "l",
-           colour="#F8766D")+
-  theme(legend.position = "bottom", 
-        legend.title = element_text(face="bold", size=10),
-        legend.key=element_rect(size=10, color=NA), 
-        legend.key.size=unit(8,"mm"),
-        legend.text=element_text(size=10), 
-        legend.direction = "horizontal",
-        legend.box = "horizontal" )+
-  stat_compare_means(method="wilcox.test", 
-                     label.x = c(1.3), 
-                     label.y = 16.1,
-                     label="p.format",
-                     size=3)
+plot_grid(g1,g2,g3,g4,ncol = 2, labels="AUTO")
+rm(list = ls())
 
-g7 <- ggplot(data_vdj_log2, aes(x=steroid, y=TRD, fill=steroid))+
-  ggtitle("TRD")+
-  geom_boxplot(width=c(0.4), lwd=0.2, outlier.size = 0.5)+
-  scale_y_continuous("TRD counts")+
-  scale_x_discrete("")+
-  scale_fill_manual(values=c("#F8766D","#00BFC4"), 
-                    labels=c("HSP (n=45)","LSP (n=30)"))+
-  geom_rug(data = data_vdj_log2[data_vdj_log2$steroid=="LSP",], 
-           aes(x=NULL), 
-           sides=  "r",
-           colour= "#00BFC4")+
-  geom_rug(data = data_vdj_log2[data_vdj_log2$steroid=="HSP",], 
-           aes(x=NULL), 
-           sides=  "l",
-           colour="#F8766D")+
-  theme(legend.position = "bottom", 
-        legend.title = element_text(face="bold", size=10),
-        legend.key=element_rect(size=10, color=NA), 
-        legend.key.size=unit(8,"mm"),
-        legend.text=element_text(size=10), 
-        legend.direction = "horizontal",
-        legend.box = "horizontal" )+
-  stat_compare_means(method="wilcox.test", 
-                     label.x = c(1.3), 
-                     label.y = 16.1,
-                     label="p.format",
-                     size=3)
-
-plot_grid(g1,g2,g3,g4,g5,g6,g7,ncol = 4, labels="AUTO")
+################################################################################
 
