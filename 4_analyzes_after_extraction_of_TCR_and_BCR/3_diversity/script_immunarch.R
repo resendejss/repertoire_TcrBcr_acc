8bef8bd667ba60b692ce555069ec9cba6b737dc9
resendejss <jean.souza@edu.unipar.br>
2023-06-21 11:34:44 -0300

    atualizacao

--- 4_analyzes_after_extraction_of_TCR_and_BCR/3_diversity/script_immunarch.R
+++ 4_analyzes_after_extraction_of_TCR_and_BCR/3_diversity/script_immunarch.R
@@ -1,3 +1,42 @@
 ################################################################################
 
-install.packages("immunarch")
+#install.packages("immunarch")
+
+library(immunarch)
+
+immdata <- repLoad("~/Documentos/outputTrust4_report/")
+load("metadata.RData")
+
+metadata$Sample <- paste(metadata$sample_id, "_report", sep = "")
+metadata <- metadata[,c(16, 1:15)]
+metadata$steroid
+metadata$Sample[is.na(metadata$steroid)]
+metadata <- metadata[!is.na(metadata$steroid),]
+metadata$steroid
+
+#metadata <- na.omit(metadata)
+immdata[[2]] <- metadata
+
+# estatistica basica
+exp_vol <- repExplore(immdata$data, .method = "volume")
+exp_len <- repExplore(immdata$data, .method = "len", .col = "aa")
+exp_cnt <- repExplore(immdata$data, .method = "count")
+
+p1 <- vis(exp_vol, .by = c("steroid"), .meta = immdata$meta)
+p2 <- vis(exp_len, .by = c("steroid"), .meta = immdata$meta, .points = F)
+p3 <- vis(exp_cnt, .by = c("steroid"), .meta = immdata$meta)
+
+# sobreposicao de repertorios
+ov <- repOverlap(immdata$data)
+vis(ov, "circos", annotationTrack="grid")
+vis_heatmap2(ov, show_rownames=T, show_colnames=F)
+
+imm_ov1 <- repOverlap(immdata$data, .method = "public", .verbose = F)
+vis_heatmap2(imm_ov1, show_rownames=F, show_colnames=F)
+vis(imm_ov1)
+
+imm_ov2 <- repOverlap(immdata$data, .method = "jaccard", .verbose = F)
+vis_heatmap2(imm_ov2, show_rownames=F, show_colnames=F)
+
+pr.aav <- pubRep(immdata$data, "aa+v", .verbose = F)
+
