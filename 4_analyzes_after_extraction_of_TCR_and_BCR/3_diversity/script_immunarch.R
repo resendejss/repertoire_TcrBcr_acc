fa8be3e4c158a5405db7a3bc8eb5d3109e17d2bf
resendejss <jean.souza@edu.unipar.br>
2023-06-22 11:32:35 -0300

    atualizacao

--- 4_analyzes_after_extraction_of_TCR_and_BCR/3_diversity/script_immunarch.R
+++ 4_analyzes_after_extraction_of_TCR_and_BCR/3_diversity/script_immunarch.R
@@ -22,6 +22,12 @@ exp_vol <- repExplore(immdata$data, .method = "volume")
 exp_len <- repExplore(immdata$data, .method = "len", .col = "aa")
 exp_cnt <- repExplore(immdata$data, .method = "count")
 
+vis(exp_vol, .by = c("steroid"), .meta = immdata$meta)
+vis(exp_vol, .by = c("gender"), .meta = immdata$meta)
+vis(exp_vol, .by = c("stage"), .meta = immdata$meta)
+vis(exp_vol, .by = c("Immune.Subtype"), .meta = immdata$meta)
+vis(exp_vol, .by = c("cortisol.excess"), .meta = immdata$meta)
+
 p1 <- vis(exp_vol, .by = c("steroid"), .meta = immdata$meta)
 p2 <- vis(exp_len, .by = c("steroid"), .meta = immdata$meta, .points = F)
 p3 <- vis(exp_cnt, .by = c("steroid"), .meta = immdata$meta)
@@ -39,4 +45,18 @@ imm_ov2 <- repOverlap(immdata$data, .method = "jaccard", .verbose = F)
 vis_heatmap2(imm_ov2, show_rownames=F, show_colnames=F)
 
 pr.aav <- pubRep(immdata$data, "aa+v", .verbose = F)
-write.csv(pr.aav, file="")
+write.csv(pr.aav, file="pr.avv.csv")
+
+
+clon_homeo <- repClonality(immdata$data, "clonal.prop")
+imm_rare <- repClonality(immdata$data, .method = "rare")
+vis(imm_rare)
+vis(imm_rare, .by = "steroid", .meta = immdata$meta)
+
+imm_top <- repClonality(immdata$data, .method = "top", .head = c(10, 100, 1000, 3000))
+vis(imm_top, .by = "steroid", .meta = immdata$meta)
+
+imm_hom <- repClonality(immdata$data, .method = "homeo")
+vis(imm_hom)
+vis(imm_hom, .by = "steroid", .meta = immdata$meta)
+
